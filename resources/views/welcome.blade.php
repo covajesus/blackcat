b50c602e78bc4147ef61df39cf5bb665786201fe
covajesus <jesuscova@jisparking.com>
2025-11-10 21:51:45 -0300

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,12 @@
     <div class="slider">
         <div id="rev-slider-1" class="rev_slider gradient-slider" style="display:none" data-version="5.4.5">
             <ul>
+                <!-- SLIDE OFERTA COMPARTIDA -->
                 <li data-transition="crossfade">
                     <!-- MAIN IMAGE -->
-                    <img src="{{ asset('public/frontend/images/slider/slider4.jpg') }}" 
-                         alt="{{ __('messages.slider_hostal_alt') }}" 
-                         title="{{ __('messages.slider_hostal_alt') }}" 
+                    <img src="{{ asset('public/frontend/images/slider/oferta-compartida.jpg') }}" 
+                         alt="Oferta habitación compartida" 
+                         title="Oferta habitación compartida" 
                          data-bgposition="center center" 
                          data-bgfit="cover" 
                          data-bgrepeat="no-repeat" 
@@ -17,170 +18,22 @@
                          class="rev-slidebg" 
                          data-no-retina="" 
                          loading="eager">
-                    
-                    <!-- LAYER NR. 1 -->
-                    <h1 class="tp-caption tp-resizeme"
-                        data-x="center"
-                        data-hoffset=""
-                        data-y="320"
-                        data-voffset=""
-                        data-responsive_offset="on"
-                        data-fontsize="['80','50','40','30']"
-                        data-lineheight="['60','50','40','30']"
-                        data-whitespace="nowrap"
-                        data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                        style="z-index: 5; color: #fff; font-weight: 900;">
-                        {{ __('messages.welcome_title') }}
-                    </h1>
-                    
-                    <!-- LAYER NR. 2 -->
-                    <div
-                class="tp-caption tp-resizeme"
-                data-x="center"
-                data-hoffset=""
-                data-y="410"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-fontsize="16"
-                data-lineheight="16"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 6; color: #fff;">{{ __('messages.welcome_subtitle') }}</div>
-              <!-- LAYER NR. 3 -->
-              <div
-                class="tp-caption"
-                data-x="center"
-                data-hoffset="-120"
-                data-y="480"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":2400,"speed":1500,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 11;">
-              </div>
-              <!-- LAYER NR. 4 -->
-              <div
-                class="tp-caption"
-                data-x="center"
-                data-hoffset="128"
-                data-y="480"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":2400,"speed":1500,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 11;">
-              </div>
-              <!-- LAYER NR. 5 -->
-            </li>
-            <!-- SLIDE NR. 2 -->
-            <li data-transition="crossfade">
-              <!-- MAIN IMAGE -->
-              <img src="{{ asset('public/frontend/images/slider/slider2.jpg') }}" alt="{{ __('messages.slider_rooms_alt') }}" title="{{ __('messages.slider_rooms_alt') }}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina="" loading="eager">
-              <!-- LAYER NR. 1 -->
-              <div
-                class="tp-caption tp-resizeme"
-                data-x="center"
-                data-hoffset=""
-                data-y="320"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-fontsize="['70','50','40','25']"
-                data-lineheight="['60','50','40','25']"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 5; color: #fff; font-weight: 900;">{{ __('messages.slide2_title') }}
-              </div>
-              <!-- LAYER NR. 2 -->
-              <div
-                class="tp-caption tp-resizeme"
-                data-x="center"
-                data-hoffset=""
-                data-y="410"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-fontsize="16"
-                data-lineheight="16"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 6; color: #fff;">{{ __('messages.slide2_subtitle') }}
-              </div>
-            </li>
-            <!-- SLIDE NR. 2 -->
-            <li data-transition="crossfade">
-              <!-- MAIN IMAGE -->
-              <img src="{{ asset('public/frontend/images/slider/slider3.jpg') }}" alt="{{ __('messages.slider_culture_alt') }}" title="{{ __('messages.slider_culture_alt') }}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina="" loading="eager">
-              <!-- LAYER NR. 1 -->
-              <div
-                class="tp-caption tp-resizeme"
-                data-x="center"
-                data-hoffset=""
-                data-y="320"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-fontsize="['70','50','40','25']"
-                data-lineheight="['60','50','40','25']"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 5; color: #fff; font-weight: 900;">{{ __('messages.slide3_title') }}
-              </div>
-              <!-- LAYER NR. 2 -->
-              <div
-                class="tp-caption tp-resizeme"
-                data-x="center"
-                data-hoffset=""
-                data-y="410"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-fontsize="16"
-                data-lineheight="16"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 6; color: #fff;">{{ __('messages.slide3_subtitle') }}
-              </div>
-              <!-- SLIDE NR. 2 -->
-            <li data-transition="crossfade">
-              <!-- MAIN IMAGE -->
-              <img src="{{ asset('public/frontend/images/slider/slider1.jpg') }}" alt="{{ __('messages.slider_hostal_alt') }}" title="{{ __('messages.slider_hostal_alt') }}" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina="" loading="eager">
-              <!-- LAYER NR. 1 -->
-              <div
-                class="tp-caption tp-resizeme"
-                data-x="center"
-                data-hoffset=""
-                data-y="320"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-fontsize="['70','50','40','25']"
-                data-lineheight="['60','50','40','25']"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 5; color: #fff; font-weight: 900;">{{ __('messages.slide4_title') }}
-              </div>
-              <!-- LAYER NR. 2 -->
-              <div
-                class="tp-caption tp-resizeme"
-                data-x="center"
-                data-hoffset=""
-                data-y="410"
-                data-voffset=""
-                data-responsive_offset="on"
-                data-fontsize="16"
-                data-lineheight="16"
-                data-whitespace="nowrap"
-                data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":500,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 6; color: #fff;">{{ __('messages.slide4_subtitle') }}
-              </div>
-            </li>
-            <!-- SLIDE OFERTA COMPARTIDA -->
-            <li data-transition="crossfade">
-              <!-- MAIN IMAGE -->
-              <img src="{{ asset('public/frontend/images/slider/oferta-compartida.jpg') }}" alt="Oferta habitación compartida" title="Oferta habitación compartida" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina="" loading="eager">
-            </li>
-            <!-- SLIDE OFERTA MATRIMONIAL -->
-            <li data-transition="crossfade">
-              <!-- MAIN IMAGE -->
-              <img src="{{ asset('public/frontend/images/slider/oferta-matrimonial.jpg') }}" alt="Oferta habitación matrimonial" title="Oferta habitación matrimonial" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="10" class="rev-slidebg" data-no-retina="" loading="eager">
-            </li>
-          </ul>
+                </li>
+                <!-- SLIDE OFERTA MATRIMONIAL -->
+                <li data-transition="crossfade">
+                    <!-- MAIN IMAGE -->
+                    <img src="{{ asset('public/frontend/images/slider/oferta-matrimonial.jpg') }}" 
+                         alt="Oferta habitación matrimonial" 
+                         title="Oferta habitación matrimonial" 
+                         data-bgposition="center center" 
+                         data-bgfit="cover" 
+                         data-bgrepeat="no-repeat" 
+                         data-bgparallax="10" 
+                         class="rev-slidebg" 
+                         data-no-retina="" 
+                         loading="eager">
+                </li>
+            </ul>
         </div>
         <!--Inicio Codigo FnsRooms-->
           <div id="form_reservas" class="detail_room" style="padding: 0px; margin-top: -60px; text-align:center !important;"></div>
